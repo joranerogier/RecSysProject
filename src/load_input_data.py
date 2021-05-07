f80eb38ee23fc5fee5ae4d486109cc37346d626d
Jorane Rogier <jorane_rogier@hotmail.com>
2021-05-07 18:05:43 +0200

    alpha values added in plot configuration

--- src/load_input_data.py
+++ src/load_input_data.py
@@ -24,6 +24,8 @@ class InputDataLoader():
     def load_sparse_data(self):
         if(self.input_data == "own"):
             user_item_matrix = pd.read_csv(self.input_path, sep=',', encoding="latin-1")
+            user_item_matrix = user_item_matrix.set_index('user')
+            print(user_item_matrix)
             #user_item_matrix = transform_dense_to_sparse_data(ratings)
         else:
             ml100k = ML100K('ml-100k')
